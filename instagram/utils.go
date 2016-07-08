1964b8d9b7bfd359ef2d53651a959541f8d8a47d
boston <bost56@gmail.com>
2016-07-08 15:47:48 +0500

    add test, fux GetProfileByURL

--- instagram/utils.go
+++ instagram/utils.go
@@ -1,19 +1,39 @@
 package instagram
 
 import (
+	"errors"
 	"net/url"
 	"strings"
 )
 
-// GetProfileByUrl - получение чистого навания профиля из произвольного URL
-func GetProfileByUrl(profileURL string) (profileName *string, err error) {
+// GetProfileByURL - получение чистого навания профиля из произвольного URL
+func GetProfileByURL(profileURL string) (profileName *string, err error) {
+
+	if strings.Index(profileURL, "//") == 0 {
+		profileURL = "https:" + profileURL
+	}
+	if strings.Index(profileURL, "://") == -1 {
+		profileURL = "https://" + profileURL
+	}
 
 	u, err := url.Parse(profileURL)
 	if err != nil {
 		return nil, err
 	}
 
-	profileName = &strings.Split(strings.Trim(u.Path, "/"), "/")[0]
+	if "instagram.com" != u.Host {
+		return nil, errors.New("Host error")
+	}
 
-	return
+	var profile string
+
+	profile = strings.Split(strings.Trim(u.Path, "/"), "/")[0]
+	profile = strings.TrimSpace(profile)
+
+	if profile == "" {
+		return nil, errors.New("Empty profile")
+
+	}
+
+	return &profile, nil
 }
---
+++ instagram/utils_test.go
@@ -0,0 +1,45 @@
+package instagram_test
+
+import (
+	"testing"
+
+	"github.com/xboston/go-instagram/instagram"
+)
+
+func TestGetProfileByURL(t *testing.T) {
+	tests := []struct {
+		in  string
+		out string
+		err bool
+	}{
+		{in: "", err: true},
+		{in: "/", err: true},
+		{in: "//", err: true},
+
+		{in: "http://no-instagram.com", err: true},
+		{in: "http://instagram.com", err: true},
+		{in: "HTTP://x.instagram.com", err: true},
+		{in: "HTTP://x.instagram.com/username", err: true},
+		{in: "HTTP://instagram.com/username", out: "username"},
+		{in: "HTTPs://instagram.com/username", out: "username"},
+		{in: "instagram.com/username", out: "username"},
+		{in: "//instagram.com/username", out: "username"},
+	}
+
+	for _, tt := range tests {
+		profile, err := instagram.GetProfileByURL(tt.in)
+		if err != nil {
+			if !tt.err {
+				t.Errorf(`"%s": unexpected error "%v"`, tt.in, err)
+			}
+			continue
+		}
+		if tt.err && err == nil {
+			t.Errorf(`"%s": expected error`, tt.in)
+			continue
+		}
+		if *profile != tt.out {
+			t.Errorf(`"%s": got "%s", want "%v"`, tt.in, *profile, tt.out)
+		}
+	}
+}
